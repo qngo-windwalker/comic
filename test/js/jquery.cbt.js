c6c709bd7d7c163d432d1b45f0ed2262eeaf9845
Quan-home pc <quancreative@gmail.com>
2013-02-05 00:53:08 -0500

    clean up.

--- test/js/jquery.cbt.js
+++ test/js/jquery.cbt.js
@@ -91,21 +91,22 @@ $(document).ready(function () {
 
 var MainScroll = (function () {
 	var w = $("#cbt-player-container");
-    var HEIGHT = w.height();
-    var WIDTH = w.width();
     var clampWidth = w.width();
     var clampHeight = w.height();
+   
+    
+    console.log("clampHeight : " + clampHeight);
+    
     var settings = {}, defaults = {
         startAt: 0,
         sectionCheckInterval: clampHeight,
         clampWidth: clampWidth,
         tracking: false,
         firstPart: clampHeight * 6.2,
-        maxScroll: clampHeight * 30.3
     }, scrollContoller;
-    var wHeight;
-    var $scrollBar, $scrollThumb, isScrolling, scrollBarHeight, scrollThumbHeight, thumbDelta, scrollThumbPosition, scrollPercent;
-    var maxScroll = clampHeight * 30.3;
+    
+    
+    var isScrolling, thumbDelta, scrollThumbPosition;
     var isMobile = navigator.userAgent.match(/android|avantgo|android|Android|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od|ad)|iris|kindle|lge |maemo|meego.+mobile|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i) != null;
     if (isMobile == false) {
         var musicVolume = 20;
@@ -163,105 +164,7 @@ var MainScroll = (function () {
     var firstPart = clampHeight * 6.2;
     var animation = getAnimation();
 
-    function getCurrent(cur, top, bottom, min, max) {
-        return (max - min) / (bottom - top) * (cur - top) + min;
-    }
-
-    function reposBackground(th) {
-        var backgroundArray = [{
-            element: "#section0",
-            width: 1280,
-            height: 996
-        }, {
-            element: ".shuffle-bg",
-            width: 1280,
-            height: 720,
-            image: true
-        }, {
-            element: "#section1-bg",
-            width: 132,
-            height: 120,
-            image: true
-        }, {
-            element: "#section2-bg",
-            width: 132,
-            height: 120,
-            image: true
-        }, {
-            element: "#section3-bg",
-            width: 1332,
-            height: 720,
-            image: true
-        }, {
-            element: "#section4-bg",
-            width: 1332,
-            height: 720,
-            image: true
-        }, {
-            element: "#section5-bg",
-            width: 1280,
-            height: 760,
-            image: true
-        }, {
-            element: "#final-bg",
-            width: 1280,
-            height: 720,
-            image: true
-        }];
-        for (var i = 0; i < backgroundArray.length; i++) {
-            var stageW = StageController.sceneWidth;
-            var sPer = stageW / th;
-            var imgPer = backgroundArray[i].width / backgroundArray[i].height;
-            var sW;
-            var sH;
-            var imgX;
-            var imgY;
-            if (imgPer > sPer) {
-                sW = Math.round(backgroundArray[i].width * (th / backgroundArray[i].height));
-                sH = th;
-                imgX = Math.round((stageW - sW) / 2);
-                imgY = 0;
-            } else {
-                sW = stageW;
-                sH = Math.round(backgroundArray[i].height * (stageW / backgroundArray[i].width));
-                imgX = 0;
-                imgY = Math.round((th - sH) / 2);
-            }
-            if (backgroundArray[i].ratio) {
-                sW = sW * backgroundArray[i].ratio;
-                sH = sH * backgroundArray[i].ratio;
-            }
-            if (backgroundArray[i].image && backgroundArray[i].image == true) {
-                $(backgroundArray[i].element).css({
-                    width: sW + "px",
-                    height: sH + "px",
-                    top: imgY,
-                    left: imgX
-                });
-            } else {
-                $(backgroundArray[i].element).css({
-                    "background-size": sW + "px " + sH + "px"
-                });
-            }
-            if (backgroundArray[i].element == "#section19-bg") {
-                $("#section19-window").css({
-                    "top": (sH / 2),
-                    "margin-top": -sH * 0.52,
-                    "margin-left": sW * 0.11,
-                    "height": 107 * (sH / backgroundArray[i].height),
-                    "width": 110 * (sW / backgroundArray[i].width)
-                });
-            }
-        }
-    }
-
-    function isTouch() {
-        return 'ontouchstart' in window;
-    }
-    
     function getAnimation() {
-        // firstPart = clampHeight * 6.2
-        // maxScroll: clampHeight * 6.3
         var animation = [{
             selector: '#verticalScrollArea',
             startAt: 0,
@@ -280,82 +183,6 @@ var MainScroll = (function () {
                 }
             }]
         }, {
-            selector: '#section1-text',
-            startAt: 0,
-            endAt: clampHeight * 0.4,
-            onEndAnimate: function (anim) {},
-            keyframes: [{
-                position: 0,
-                properties: {
-                    "margin-top": -70
-                }
-            }, {
-                position: 1,
-                ease: TWEEN.Easing.Linear.EaseNone,
-                properties: {
-                    "margin-top": -clampHeight
-                }
-            }]
-        }, {
-            selector: '#section1-arrow',
-            startAt: 10,
-            endAt: clampHeight * 0.22,
-            onEndAnimate: function (anim) {
-                if (isMobile == false) {
-                    // soundAuto.stop();
-                    // soundAsc.stop();
-                    // soundSport.stop();
-                }
-            },
-            keyframes: [{
-                position: 0,
-                properties: {
-                    "margin-top": 220
-                }
-            }, {
-                position: 1,
-                ease: TWEEN.Easing.Linear.EaseNone,
-                properties: {
-                    "margin-top": 480
-                }
-            }]
-        }, {
-            selector: '#section1-scroll',
-            startAt: 0,
-            endAt: clampHeight * 0.2,
-            onEndAnimate: function (anim) {},
-            keyframes: [{
-                position: 0,
-                properties: {
-                    "margin-top": 180,
-                    "opacity": 1
-                }
-            }, {
-                position: 1,
-                ease: TWEEN.Easing.Linear.EaseNone,
-                properties: {
-                    "margin-top": 0,
-                    "opacity": 0
-                }
-            }]
-        }, {
-            selector: '#section1-better',
-            startAt: 0,
-            endAt: clampHeight * 0.2,
-            onEndAnimate: function (anim) {},
-            keyframes: [{
-                position: 0,
-                properties: {
-                    "margin-top": -22
-                }
-            }, {
-                position: 1,
-                ease: TWEEN.Easing.Linear.EaseNone,
-                properties: {
-                    "margin-top": 40
-                }
-            }]
-        }, {
             selector: '#section1',
             startAt: 0,
             endAt: clampHeight * 0.6,
@@ -363,7 +190,7 @@ var MainScroll = (function () {
             keyframes: [{
                 position: 0,
                 properties: {
-                    "margin-top": 10
+                    "margin-top": 0
                 }
             }, {
                 position: 1,
@@ -653,41 +480,6 @@ var MainScroll = (function () {
                 }
             }]
         }, {
-            startAt: clampHeight * 0.7,
-            endAt: clampHeight * 0.8,
-            onEndAnimate: function (anim) {
-            }
-        }, {
-            startAt: clampHeight * 1.8,
-            endAt: clampHeight * 1.9,
-            onEndAnimate: function (anim) {
-            }
-        }, {
-            startAt: clampHeight * 3.6,
-            endAt: clampHeight * 3.7,
-            onEndAnimate: function (anim) {
-            }
-        }, {
-            startAt: clampHeight * 4.9,
-            endAt: clampHeight * 5,
-            onEndAnimate: function (anim) {
-            }
-        }, {
-            startAt: clampHeight * 10.3,
-            endAt: clampHeight * 10.4,
-            onEndAnimate: function (anim) {
-            }
-        }, {
-            startAt: clampHeight * 13.7,
-            endAt: clampHeight * 13.8,
-            onEndAnimate: function (anim) {
-            }
-        }, {
-            startAt: clampHeight * 16.5,
-            endAt: clampHeight * 16.6,
-            onEndAnimate: function (anim) {
-            }
-        }, {
             startAt: clampHeight * 0.51,
             endAt: clampHeight * 0.52,
             onEndAnimate: function (anim) {
@@ -703,31 +495,6 @@ var MainScroll = (function () {
         }, {
             startAt: clampHeight * 4.58,
             endAt: clampHeight * 4.59,
-            onEndAnimate: function (anim) {
-            }
-        }, {
-            startAt: clampHeight * 7.46,
-            endAt: clampHeight * 7.47,
-            onEndAnimate: function (anim) {
-            }
-        }, {
-            startAt: clampHeight * 8.4,
-            endAt: clampHeight * 8.41,
-            onEndAnimate: function (anim) {
-            }
-        }, {
-            startAt: 0,
-            endAt: 0.01,
-            onEndAnimate: function (anim) {
-            }
-        }, {
-            startAt: clampHeight * 4.91,
-            endAt: clampHeight * 4.92,
-            onEndAnimate: function (anim) {
-            }
-        }, {
-            startAt: clampHeight * 6.91,
-            endAt: clampHeight * 6.92,
             onEndAnimate: function (anim) {
             }
         }];
@@ -747,13 +514,6 @@ var MainScroll = (function () {
         return (max - min) / (bottom - top) * (cur - top) + min;
     }
     
-   
-    function resizeScrollBar() {
-        scrollBarHeight = wHeight;
-        $scrollBar.css('height', scrollBarHeight + "px");
-        setScrollBarPosition(scrollPercent);
-    }
-
     function startScroll(event) {
         isScrolling = true;
         thumbDelta = scrollThumbPosition - event.pageY;
@@ -762,11 +522,6 @@ var MainScroll = (function () {
         return false;
     }
 
-   
-    function setScrollBarPosition(percent) {
-        scrollThumbPosition = (scrollBarHeight - scrollThumbHeight) * percent;
-        $scrollThumb.css('top', scrollThumbPosition);
-    }
 
     function endScroll(event) {
         isScrolling = false;
@@ -780,10 +535,7 @@ var MainScroll = (function () {
     	console.log('Main scroll init');
     	
         settings = $.extend(defaults, opts);
-        var scroll_speed;
-            scroll_speed = 30;
-            // $("#scrollBar").show();
-            
+            // $("#scrollBar").show();            
             //activateScrollBar(40);
             
         var directionTouch = [{
@@ -810,47 +562,24 @@ var MainScroll = (function () {
         
         scrollContoller = ScrollController();
         
+        // This cannot be calculated since there's no way of monitoring assets load completed.
+
         scrollContoller.init({
-        	// debugId: true,
+        	debugId: true,
             animation: animation,
-            maxScroll: maxScroll,
+            // This cannot be calculated since there's no way of monitoring assets load completed.
+            // 760 is the image's height multiple by 5
+            maxScroll: 760 * 5,
             tickSpeed: 20,
             directionTouch: directionTouch,
-            scrollSpeed: scroll_speed,
+            scrollSpeed: 30,
             tweenSpeed: .4,
             startAt: settings.startAt,
             container: $('#main'),
             onStart: function () {},
-            onResize: function (page) {
-                w = $("#container");
-                HEIGHT = w.height();
-                WIDTH = w.width();
-                clampWidth = w.width();
-                clampHeight = w.height();
-                sectionHeight = HEIGHT;
-                wHeight = HEIGHT;
-                settings = {}, defaults = {
-                    startAt: 0,
-                    sectionCheckInterval: clampHeight,
-                    clampWidth: clampWidth,
-                    tracking: false,
-                    firstPart: clampHeight * 6.2,
-                    maxScroll: clampHeight * 30.8
-                }, scrollContoller;
-                maxScroll = clampHeight * 30.3;
-                firstPart = clampHeight * 6.2;
-                reposBackground(HEIGHT);
-                if (clampHeight < 740) {
-                    $("#section2-girl").css({
-                        bottom: -(740 - clampHeight)
-                    });
-                }
-                if ($scrollBar) resizeScrollBar();
-                scrollContoller.scrollTo(scrollContoller.getScrollTop() + 3);
-            },
+            onResize: function (page) {},
             onUpdate: function (scrollTop) {
                 if (StageController.isDebug) $('#scrollTopTweened').html(("" + scrollTop).substring(0, 10));
-                if ($scrollBar) setScrollBarPosition(scrollTop / maxScroll);
             }
         });
         
@@ -870,7 +599,6 @@ var MainScroll = (function () {
         {
             scrollContoller.scrollTo(3106);
         });
-        
         
 		$('#section1-start').click(function(e) {
 			e.preventDefault();
